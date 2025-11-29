95defbef25b4bab55376f082700adbb43f946d45
Hamza <hamzaaddami11@gmail.com>
2025-11-29 19:04:47 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace Chatup.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly Guid Id = Guid.NewGuid();
+
+        [HttpGet]
+        public IActionResult GetId()
+        {
+            string response = this.Id.ToString();
+            return Ok(response);
+        }
+
+    }
+}
